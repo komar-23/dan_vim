b578a26b8386b90090c8e2a4579683f3292f97ab
komar-23 <komar_23@wp.pl>
2025-05-27 20:56:25 +0200

    Correction of installVimPlugins script.

--- installVimPlugins.sh
+++ installVimPlugins.sh
@@ -1,34 +1,75 @@
 #!/bin/bash
 #Script prepares my custom VIM distribution
 
-VIM_PACK_DIR='$VIM_PACK_DIR'
+VIM_PACK_DIR='.vimtmp/pack'
 
-mkdir -p "~/$VIM_PACK_DIR/vendor"
-mkdir -p '~/$VIM_PACK_DIR/mine'
-mkdir -p '~/$VIM_PACK_DIR/mine/opt'
-mkdir -p '~/$VIM_PACK_DIR/mine/start'
-mkdir -p '~/$VIM_PACK_DIR/vendor/opt'
-mkdir -p '~/$VIM_PACK_DIR/vendor/start'
+if [ ! -d "$HOME/$VIM_PACK_DIR/mine/opt" ] ; then
+  mkdir -p "$HOME/$VIM_PACK_DIR/mine/opt"
+fi
 
-mkdir '~/$VIM_PACK_DIR/vendor/start/leverf'
-git clone https://github.com/rhysd/clever-f.vim.git '~/$VIM_PACK_DIR/vendor/start/cleverf'
-mkdir '~/$VIM_PACK_DIR/vendor/start/commentary'
-git clone https://github.com/tpope/vim-commentary.git '~/$VIM_PACK_DIR/vendor/start/commentary'
-mkdir '~/$VIM_PACK_DIR/vendor/start/lastplace'
-git clone https://github.com/farmergreg/vim-lastplace.git '~/$VIM_PACK_DIR/vendor/start/lastplace'
-mkdir '~/$VIM_PACK_DIR/vendor/start/lightline'
-git clone https://github.com/itchyny/lightline.vim.git '~/$VIM_PACK_DIR/vendor/start/lightline'
-mkdir '~/$VIM_PACK_DIR/vendor/start/nerdtree'
-git clone https://github.com/preservim/nerdtree.git '~/$VIM_PACK_DIR/vendor/start/nerdtree'
-mkdir '~/$VIM_PACK_DIR/vendor/start/signature'
-git clone https://github.com/kshenoy/vim-signature.git '~/$VIM_PACK_DIR/vendor/start/signature'
-mkdir '~/$VIM_PACK_DIR/vendor/start/sort-motion'
-git clone https://github.com/christoomey/vim-sort-motion.git '~/$VIM_PACK_DIR/vendor/start/sort-motion'
-mkdir '~/$VIM_PACK_DIR/vendor/start/undotree'
-git clone https://github.com/mbbill/undotree.git '~/$VIM_PACK_DIR/vendor/start/undotree'
-mkdir '~/$VIM_PACK_DIR/vendor/start/vimwiki'
-git clone https://github.com/vimwiki/vimwiki.git '~/$VIM_PACK_DIR/vendor/start/vimwiki'
-mkdir '~/$VIM_PACK_DIR/vendor/start/surround'
-git clone https://github.com/tpope/vim-surround.git '~/$VIM_PACK_DIR/vendor/start/surround'
-mkdir '~/$VIM_PACK_DIR/vendor/start/grubbox'
-git clone https://github.com/morhetz/gruvbox.git '~/$VIM_PACK_DIR/vendor/start/gruvbox'
+if [ ! -d "$HOME/$VIM_PACK_DIR/mine/start" ] ; then
+  mkdir -p "$HOME/$VIM_PACK_DIR/mine/start"
+fi
+
+if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/opt" ] ; then
+  mkdir -p "$HOME/$VIM_PACK_DIR/vendor/opt"
+fi
+
+if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start" ] ; then
+  mkdir -p "$HOME/$VIM_PACK_DIR/vendor/start"
+fi
+
+if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/cleverf" ] ; then
+  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/cleverf"
+  git clone https://github.com/rhysd/clever-f.vim.git "$HOME/$VIM_PACK_DIR/vendor/start/cleverf"
+fi
+
+if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/commentary" ] ; then
+  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/commentary"
+  git clone https://github.com/tpope/vim-commentary.git "$HOME/$VIM_PACK_DIR/vendor/start/commentary"
+fi
+
+if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/lastplace" ] ; then
+  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/lastplace"
+  git clone https://github.com/farmergreg/vim-lastplace.git "$HOME/$VIM_PACK_DIR/vendor/start/lastplace"
+fi
+
+if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/lightline" ] ; then
+  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/lightline"
+  git clone https://github.com/itchyny/lightline.vim.git "$HOME/$VIM_PACK_DIR/vendor/start/lightline"
+fi
+
+if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/nerdtree" ] ; then
+  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/nerdtree"
+  git clone https://github.com/preservim/nerdtree.git "$HOME/$VIM_PACK_DIR/vendor/start/nerdtree"
+fi
+
+if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/signature" ] ; then
+  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/signature"
+  git clone https://github.com/kshenoy/vim-signature.git "$HOME/$VIM_PACK_DIR/vendor/start/signature"
+fi
+
+if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/sort-motion" ] ; then
+  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/sort-motion"
+  git clone https://github.com/christoomey/vim-sort-motion.git "$HOME/$VIM_PACK_DIR/vendor/start/sort-motion"
+fi
+
+if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/undotree" ] ; then
+  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/undotree"
+  git clone https://github.com/mbbill/undotree.git "$HOME/$VIM_PACK_DIR/vendor/start/undotree"
+fi
+
+if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/vimwiki" ] ; then
+  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/vimwiki"
+  git clone https://github.com/vimwiki/vimwiki.git "$HOME/$VIM_PACK_DIR/vendor/start/vimwiki"
+fi
+
+if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/surround" ] ; then
+  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/surround"
+  git clone https://github.com/tpope/vim-surround.git "$HOME/$VIM_PACK_DIR/vendor/start/surround"
+fi
+
+if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/gruvbox" ] ; then
+  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/gruvbox"
+  git clone https://github.com/morhetz/gruvbox.git "$HOME/$VIM_PACK_DIR/vendor/start/gruvbox"
+fi
