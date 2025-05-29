2a887d591d01a52d7e2e147693202f5c77cfffd8
komar-23 <komar_23@wp.pl>
2025-05-29 15:28:51 +0200

    Installation script improvment.

--- installVimPlugins.sh
+++ installVimPlugins.sh
@@ -1,75 +1,113 @@
 #!/bin/bash
 #Script prepares my custom VIM distribution
 
-VIM_PACK_DIR='.vimtmp/pack'
+VIM_PACK_DIR='.vim/pack'
 
-if [ ! -d "$HOME/$VIM_PACK_DIR/mine/opt" ] ; then
-  mkdir -p "$HOME/$VIM_PACK_DIR/mine/opt"
+if [ ! -d "${HOME}/${VIM_PACK_DIR}/mine/opt" ] ; then
+  mkdir -p "${HOME}/${VIM_PACK_DIR}/mine/opt"
+else
+  echo "${HOME}/${VIM_PACK_DIR}/mine/opt exist."
 fi
 
-if [ ! -d "$HOME/$VIM_PACK_DIR/mine/start" ] ; then
-  mkdir -p "$HOME/$VIM_PACK_DIR/mine/start"
+if [ ! -d "${HOME}/${VIM_PACK_DIR}/mine/start" ] ; then
+  mkdir -p "${HOME}/${VIM_PACK_DIR}/mine/start"
+else
+  echo "${HOME}/${VIM_PACK_DIR}/mine/start exist."
 fi
 
-if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/opt" ] ; then
-  mkdir -p "$HOME/$VIM_PACK_DIR/vendor/opt"
+if [ ! -d "${HOME}/${VIM_PACK_DIR}/vendor/opt" ] ; then
+  mkdir -p "${HOME}/${VIM_PACK_DIR}/vendor/opt"
+else
+  echo "${HOME}/${VIM_PACK_DIR}/vendor/opt exist."
 fi
 
-if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start" ] ; then
-  mkdir -p "$HOME/$VIM_PACK_DIR/vendor/start"
+if [ ! -d "${HOME}/${VIM_PACK_DIR}/vendor/start" ] ; then
+  mkdir -p "${HOME}/${VIM_PACK_DIR}/vendor/start"
+else
+  echo "${HOME}/${VIM_PACK_DIR}/vendor/start exist."
 fi
 
-if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/cleverf" ] ; then
-  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/cleverf"
-  git clone https://github.com/rhysd/clever-f.vim.git "$HOME/$VIM_PACK_DIR/vendor/start/cleverf"
+if [ ! -d "${HOME}/${VIM_PACK_DIR}/vendor/start/cleverf" ] ; then
+  mkdir "${HOME}/${VIM_PACK_DIR}/vendor/start/cleverf"
+  git clone https://github.com/rhysd/clever-f.vim.git "${HOME}/${VIM_PACK_DIR}/vendor/start/cleverf"
+else
+  echo "${HOME}/${VIM_PACK_DIR}/vendor/start/cleverf exist."
 fi
 
-if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/commentary" ] ; then
-  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/commentary"
-  git clone https://github.com/tpope/vim-commentary.git "$HOME/$VIM_PACK_DIR/vendor/start/commentary"
+if [ ! -d "${HOME}/${VIM_PACK_DIR}/vendor/start/commentary" ] ; then
+  mkdir "${HOME}/${VIM_PACK_DIR}/vendor/start/commentary"
+  git clone https://github.com/tpope/vim-commentary.git "${HOME}/${VIM_PACK_DIR}/vendor/start/commentary"
+else
+  echo "${HOME}/${VIM_PACK_DIR}/vendor/start/commentary exist."
 fi
 
-if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/lastplace" ] ; then
-  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/lastplace"
-  git clone https://github.com/farmergreg/vim-lastplace.git "$HOME/$VIM_PACK_DIR/vendor/start/lastplace"
+if [ ! -d "${HOME}/${VIM_PACK_DIR}/vendor/start/lastplace" ] ; then
+  mkdir "${HOME}/${VIM_PACK_DIR}/vendor/start/lastplace"
+  git clone https://github.com/farmergreg/vim-lastplace.git "${HOME}/${VIM_PACK_DIR}/vendor/start/lastplace"
+else
+  echo "${HOME}/${VIM_PACK_DIR}/vendor/start/lastplace exist."
 fi
 
-if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/lightline" ] ; then
-  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/lightline"
-  git clone https://github.com/itchyny/lightline.vim.git "$HOME/$VIM_PACK_DIR/vendor/start/lightline"
+if [ ! -d "${HOME}/${VIM_PACK_DIR}/vendor/start/lightline" ] ; then
+  mkdir "${HOME}/${VIM_PACK_DIR}/vendor/start/lightline"
+  git clone https://github.com/itchyny/lightline.vim.git "${HOME}/${VIM_PACK_DIR}/vendor/start/lightline"
+else
+  echo "${HOME}/${VIM_PACK_DIR}/vendor/start/lightline exist."
 fi
 
-if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/nerdtree" ] ; then
-  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/nerdtree"
-  git clone https://github.com/preservim/nerdtree.git "$HOME/$VIM_PACK_DIR/vendor/start/nerdtree"
+if [ ! -d "${HOME}/${VIM_PACK_DIR}/vendor/start/nerdtree" ] ; then
+  mkdir "${HOME}/${VIM_PACK_DIR}/vendor/start/nerdtree"
+  git clone https://github.com/preservim/nerdtree.git "${HOME}/${VIM_PACK_DIR}/vendor/start/nerdtree"
+else
+  echo "${HOME}/${VIM_PACK_DIR}/vendor/start/nerdtree exist."
 fi
 
-if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/signature" ] ; then
-  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/signature"
-  git clone https://github.com/kshenoy/vim-signature.git "$HOME/$VIM_PACK_DIR/vendor/start/signature"
+if [ ! -d "${HOME}/${VIM_PACK_DIR}/vendor/start/signature" ] ; then
+  mkdir "${HOME}/${VIM_PACK_DIR}/vendor/start/signature"
+  git clone https://github.com/kshenoy/vim-signature.git "${HOME}/${VIM_PACK_DIR}/vendor/start/signature"
+else
+  echo "${HOME}/${VIM_PACK_DIR}/vendor/start/signature exist."
 fi
 
-if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/sort-motion" ] ; then
-  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/sort-motion"
-  git clone https://github.com/christoomey/vim-sort-motion.git "$HOME/$VIM_PACK_DIR/vendor/start/sort-motion"
+if [ ! -d "${HOME}/${VIM_PACK_DIR}/vendor/start/sort-motion" ] ; then
+  mkdir "${HOME}/${VIM_PACK_DIR}/vendor/start/sort-motion"
+  git clone https://github.com/christoomey/vim-sort-motion.git "${HOME}/${VIM_PACK_DIR}/vendor/start/sort-motion"
+else
+  echo "${HOME}/${VIM_PACK_DIR}/vendor/start/sort-motion exist."
 fi
 
-if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/undotree" ] ; then
-  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/undotree"
-  git clone https://github.com/mbbill/undotree.git "$HOME/$VIM_PACK_DIR/vendor/start/undotree"
+if [ ! -d "${HOME}/${VIM_PACK_DIR}/vendor/start/undotree" ] ; then
+  mkdir "${HOME}/${VIM_PACK_DIR}/vendor/start/undotree"
+  git clone https://github.com/mbbill/undotree.git "${HOME}/${VIM_PACK_DIR}/vendor/start/undotree"
+else
+  echo "${HOME}/${VIM_PACK_DIR}/vendor/start/undotree exist."
 fi
 
-if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/vimwiki" ] ; then
-  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/vimwiki"
-  git clone https://github.com/vimwiki/vimwiki.git "$HOME/$VIM_PACK_DIR/vendor/start/vimwiki"
+if [ ! -d "${HOME}/${VIM_PACK_DIR}/vendor/start/vimwiki" ] ; then
+  mkdir "${HOME}/${VIM_PACK_DIR}/vendor/start/vimwiki"
+  git clone https://github.com/vimwiki/vimwiki.git "${HOME}/${VIM_PACK_DIR}/vendor/start/vimwiki"
+else
+  echo "${HOME}/${VIM_PACK_DIR}/vendor/start/vimwiki exist."
 fi
 
-if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/surround" ] ; then
-  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/surround"
-  git clone https://github.com/tpope/vim-surround.git "$HOME/$VIM_PACK_DIR/vendor/start/surround"
+if [ ! -d "${HOME}/${VIM_PACK_DIR}/vendor/start/surround" ] ; then
+  mkdir "${HOME}/${VIM_PACK_DIR}/vendor/start/surround"
+  git clone https://github.com/tpope/vim-surround.git "${HOME}/${VIM_PACK_DIR}/vendor/start/surround"
+else
+  echo "${HOME}/${VIM_PACK_DIR}/vendor/start/surround exist."
 fi
 
-if [ ! -d "$HOME/$VIM_PACK_DIR/vendor/start/gruvbox" ] ; then
-  mkdir "$HOME/$VIM_PACK_DIR/vendor/start/gruvbox"
-  git clone https://github.com/morhetz/gruvbox.git "$HOME/$VIM_PACK_DIR/vendor/start/gruvbox"
+if [ ! -d "${HOME}/${VIM_PACK_DIR}/vendor/start/gruvbox" ] ; then
+  mkdir "${HOME}/${VIM_PACK_DIR}/vendor/start/gruvbox"
+  git clone https://github.com/morhetz/gruvbox.git "${HOME}/${VIM_PACK_DIR}/vendor/start/gruvbox"
+else
+  echo "${HOME}/${VIM_PACK_DIR}/vendor/start/gruvbox exist."
+fi
+
+if [ ! -f "${HOME}/.vimrc" ] ; then
+  cp ./.vimrc ${HOME}/.vimrc
+else
+  echo '.vimrc file exist, backup has been created as a .vimrc.bkp file'
+  cp ${HOME}/.vimrc ${HOME}/.vimrc.bkp
+  cp ./.vimrc ${HOME}/.vimrc
 fi
